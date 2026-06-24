a9e579dcf9c7e3692840536265faaea333f87ac9
Charbel Antoun <Charbel.Antoun@EITECH.LOCAL>
2026-06-24 16:30:58 +0300

    dynamic account list

---
+++ force-app/main/default/lwc/accountLimit/accountLimit.js
@@ -0,0 +1,29 @@
+import { LightningElement, wire, track } from 'lwc';
+import getAccountList from '@salesforce/apex/AccountController.getAccountList';
+
+export default class AccountLimit extends LightningElement {
+    @track numberOfAccounts = 10; // Default fallback value
+    accounts;
+    error;
+
+    // Pass the parameter inside an object using '$variableName' to make it reactive
+    @wire(getAccountList, { recordLimit: '$numberOfAccounts' })
+    wiredAccounts({ error, data }) {
+        if (data) {
+            this.accounts = data;
+            this.error = undefined;
+        } else if (error) {
+            this.error = error;
+            this.accounts = undefined;
+        }
+    }
+
+    handleNumberChange(event) {
+        const value = event.target.value;
+        // Only update if the user entered a valid positive number
+        if (value && value > 0) {
+            this.numberOfAccounts = parseInt(value, 10);
+        }
+    }
+    
+}
